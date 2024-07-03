4fedfd49a5e8e5f9477363b523803714af7b8ab2
jihwanyoon <yjh091500@jinhak.com>
2024-07-03 19:54:57 +0900

    feat: add fastapi backend

---
+++ exp/exp1/fastapi-app/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, File
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from openai import OpenAI
+import os
+import uvicorn
+from middlewares.time import StartTimeMiddleware
+from database.mongodb import MongoDBClient
+from exceptions.loggers import MongoLogger
+from routers import stt
+from config.utils import config
+
+
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    try:
+        print("Lifespan start!")
+        app.state.mongo_client = await MongoDBClient.get_instance()
+        print("MongoDB done!")
+        await MongoLogger.initialize()
+        print("Logger done!")
+        app.state.openai_client = OpenAI(api_key=config["API"]["OPENAI_API_KEY"])
+        print("OpenAI client done")
+        print("All Set!")
+
+        yield
+    except Exception as e:
+        print(f"Error during lifespan: {e}")
+    finally:
+        print("Closing MongoDB connection...")
+        if hasattr(app.state, "mongo_client"):
+            await app.state.mongo_client.close()
+        print("All Close!")
+
+
+app = FastAPI(lifespan=app_lifespan)
+
+
+# Middleware 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(StartTimeMiddleware)
+
+# router 추가
+app.include_router(stt.router, prefix="/api/v1/stt")
+
+
+if __name__ == "__main__":
+    workers = os.cpu_count()
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=workers)
---
+++ exp/exp1/fastapi-app/routers/stt.py
@@ -0,0 +1,46 @@
+import io
+from openai import OpenAI
+from fastapi.responses import JSONResponse
+from fastapi import APIRouter, HTTPException, Request, UploadFile, File
+
+client = OpenAI()
+router = APIRouter()
+
+
+@router.post("/stt")
+async def stt_audio(request: Request, file: UploadFile = File(...)):
+    openai_client = request.app.state.openai_client
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="No selected file")
+
+    try:
+        # 파일 확장자와 MIME 타입 확인
+        if file.content_type not in [
+            "audio/flac",
+            "audio/m4a",
+            "audio/mp3",
+            "audio/mp4",
+            "audio/mpeg",
+            "audio/mpga",
+            "audio/oga",
+            "audio/ogg",
+            "audio/wav",
+            "audio/webm",
+        ]:
+            raise HTTPException(status_code=400, detail="Unsupported file format")
+
+        # 파일을 BytesIO로 변환
+        file_data = await file.read()
+        audio_bytes = io.BytesIO(file_data)
+        audio_bytes.name = file.filename
+
+        # OpenAI API로 파일 전송
+        transcription = openai_client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_bytes,
+            response_format="text",
+        )
+        return JSONResponse(content={"transcription": transcription}, status_code=200)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
