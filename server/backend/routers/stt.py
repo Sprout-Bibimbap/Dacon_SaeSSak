43dcdea5de749c8ab3bc26e4af810225f3db0d43
jihwanyoon <yjh091500@naver.com>
2024-07-06 23:12:14 +0900

    feat: modify stt api

--- server/backend/routers/stt.py
+++ server/backend/routers/stt.py
@@ -13,28 +13,7 @@ async def transcribe_audio(request: Request, file: UploadFile = File(...)):
     if not file.filename:
         raise HTTPException(status_code=400, detail="No selected file")
     try:
-        # # 파일 확장자와 MIME 타입 확인
-        # if file.content_type not in [
-        #     "audio/flac",
-        #     "audio/m4a",
-        #     "audio/mp3",
-        #     "audio/mp4",
-        #     "audio/mpeg",
-        #     "audio/mpga",
-        #     "audio/oga",
-        #     "audio/ogg",
-        #     "audio/wav",
-        #     "audio/webm",
-        # ]:
-        #     raise HTTPException(status_code=400, detail="Unsupported file format")
-
         contents = await file.read()
-
-        # root_dir = Path(__file__).resolve().parents[4]
-        # local_file_path = root_dir / "data" / "test_sound.wav"
-        # with open(local_file_path, "rb") as audio:
-        #     file_data = audio.read()
-
         audio_bytes = io.BytesIO(contents)
         audio_bytes.name = file.filename
 
@@ -46,13 +25,12 @@ async def transcribe_audio(request: Request, file: UploadFile = File(...)):
         )
 
         # Model answer
-        transcription = "대한민국의 여름을 한 문장으로 표현하면?"
         response = openai_client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {
                     "role": "system",
-                    "content": "주어진 질문에 대해서 간단하고 친절하게 대답해주세요. 반드시 한국어로 대답해주세요.",
+                    "content": "당신은 친절하고 전문적인 아동 심리 상담사입니다. 아동이 하는 말에 따라 친절하게 응답해주세요. 반드시 두 문장 이내로 짧게 대답해주세요.",
                 },
                 {"role": "user", "content": transcription},
             ],
