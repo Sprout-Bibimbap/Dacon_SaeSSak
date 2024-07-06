a842391a0629f212d13fede14936d826ddd9d7e4
jihwanyoon <yjh091500@naver.com>
2024-07-06 23:14:37 +0900

    chore: delete exp1 folder

--- exp/exp1/stt-app/config-overrides.js
+++
@@ -1,7 +0,0 @@
-const { override, addBabelPreset } = require('customize-cra');
-
-module.exports = override(
-  addBabelPreset('@babel/preset-react')
-  // 여기에 추가적인 설정을 넣을 수 있습니다.
-  // 예: addWebpackPlugin(), addWebpackAlias() 등
-);
--- exp/exp1/stt-app/src/components/RecordingIndicator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function RecordingIndicator({ isListening, isRecording }) {
-  return (
-    <div className={`recording-indicator ${isRecording ? 'active' : ''}`}>
-      {isListening ? 'Recording...' : 'Not Recording'}
-    </div>
-  );
-}
